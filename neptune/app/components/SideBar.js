7dcfffba178f8f3e25c74f9a140ba877486a6f6b
Reese Dychiao <rkd9714@nyu.edu>
2025-04-18 17:12:23 -0400

    added shadcn and starting layout

---
+++ neptune/app/components/SideBar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const SideBar = () => {
+  return (
+    <div>
+      <div>
+        <button></button>
+        <button></button>
+      </div>
+      <div></div>
+      <button></button>
+    </div>
+  );
+};
+
+export default SideBar;
